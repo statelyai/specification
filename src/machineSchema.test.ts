0706de144a3d691beb210620ad3a6d381d8da71c
David Khourshid <davidkpiano@gmail.com>
2025-10-19 19:48:33 -0400

    Improve schema

--- src/machineSchema.test.ts
+++ src/machineSchema.test.ts
@@ -1,71 +1,63 @@
-import { expect, test } from 'vitest';
+import { test, describe } from 'node:test';
+import assert from 'node:assert';
 import { machineSchema } from './machineSchema';
 
-test('machineSchema', () => {
-  expect(machineSchema).toBeDefined();
-});
-
-test('trivial machine', () => {
-  machineSchema.parse({});
-});
-
-test('all machine properties', () => {
-  machineSchema.parse({
-    id: 'test',
-    description: 'test',
-    version: 'test',
-    initial: 'test',
-    states: {
-      test: {},
-    },
+describe('machineSchema', () => {
+  test('machineSchema is defined', () => {
+    assert.ok(machineSchema);
   });
-});
 
-test('omni machine', () => {
-  machineSchema.parse({
-    id: 'test',
-    description: 'test',
-    version: 'test',
-    initial: 'test',
-    states: {
-      test: {
-        initial: 'one',
-        states: {
-          one: {
-            type: 'final',
-          },
-          two: {
-            type: 'final',
-          },
-          three: {
-            type: 'final',
-          },
-          h: {
-            type: 'history',
-            target: 'two',
-            history: 'shallow',
+  test('trivial machine', () => {
+    machineSchema.parse({});
+  });
+
+  test('all machine properties', () => {
+    machineSchema.parse({
+      id: 'test',
+      description: 'test',
+      version: 'test',
+      initial: 'test',
+      states: {
+        test: {},
+      },
+    });
+  });
+
+  test('omni machine', () => {
+    machineSchema.parse({
+      id: 'test',
+      description: 'test',
+      version: 'test',
+      initial: 'test',
+      states: {
+        test: {
+          initial: 'one',
+          states: {
+            one: {
+              type: 'final',
+            },
+            two: {
+              type: 'final',
+            },
+            three: {
+              type: 'final',
+            },
+            h: {
+              type: 'history',
+              target: 'two',
+              history: 'shallow',
+            },
           },
         },
       },
-    },
+    });
   });
-});
 
-test('invalid machine', () => {
-  expect(() =>
-    machineSchema.parse({
-      id: 3,
-    })
-  ).toThrowErrorMatchingInlineSnapshot(`
-    [ZodError: [
-      {
-        "expected": "string",
-        "code": "invalid_type",
-        "path": [
-          "id"
-        ],
-        "message": "Invalid input: expected string, received number"
-      }
-    ]]
-  `);
+  test('invalid machine', () => {
+    assert.throws(() =>
+      machineSchema.parse({
+        id: 3,
+      })
+    );
+  });
 });
--- src/machineSchema.ts
+++ src/machineSchema.ts
@@ -1,15 +1,8 @@
-import z from 'zod';
+import z, { array } from 'zod';
 
 export const actionSchema = z.object({
   type: z.string(),
   params: z.record(z.string(), z.any()).optional(),
-});
-
-export const invokeSchema = z.object({
-  id: z.string().optional(),
-  src: z.string(),
-  // input: z.record(z.string(), z.any()).optional(),
-  meta: z.record(z.string(), z.any()).optional(),
 });
 
 export const guardSchema = z.object({
@@ -27,42 +20,18 @@ export const transitionSchema = z.object({
   meta: metaSchema.optional(),
 });
 
+export const invokeSchema = z.object({
+  id: z.string().optional(),
+  src: z.string(),
+  // input: z.record(z.string(), z.any()).optional(),
+  meta: z.record(z.string(), z.any()).optional(),
+  onDone: z.union([z.array(transitionSchema), transitionSchema]).optional(),
+  onError: z.union([z.array(transitionSchema), transitionSchema]).optional(),
+});
+
 const baseState = z.object({
   id: z.string().optional(),
   description: z.string().optional(),
-});
-
-export const atomicState = baseState.extend({
-  type: z.literal('atomic').optional(),
-  entry: z.array(actionSchema).optional(),
-  exit: z.array(actionSchema).optional(),
-  on: z.record(z.string(), transitionSchema).optional(),
-  after: z.record(z.string(), transitionSchema).optional(),
-  always: z.array(transitionSchema).optional(),
-  invoke: z.array(invokeSchema).optional(),
-});
-
-export const compoundState = baseState.extend({
-  type: z.literal('compound').optional(),
-  entry: z.array(actionSchema).optional(),
-  exit: z.array(actionSchema).optional(),
-  initial: z.string(),
-});
-
-export const parallelState = baseState.extend({
-  type: z.literal('parallel').optional(),
-  entry: z.array(actionSchema).optional(),
-  exit: z.array(actionSchema).optional(),
-});
-
-export const historyState = baseState.extend({
-  type: z.literal('history').optional(),
-  history: z.enum(['shallow', 'deep']).optional(),
-  target: z.string().optional(),
-});
-
-export const finalState = baseState.extend({
-  type: z.literal('final').optional(),
 });
 
 export const stateSchema = z.object({
@@ -81,17 +50,17 @@ export const stateSchema = z.object({
   exit: z.array(actionSchema).optional().describe('The exit actions'),
   initial: z.string().optional().describe('The initial child state'),
   on: z
-    .record(z.string(), transitionSchema)
+    .record(z.string(), z.union([z.array(transitionSchema), transitionSchema]))
     .optional()
     .describe('The transitions'),
   after: z
-    .record(z.string(), transitionSchema)
+    .record(z.string(), z.union([z.array(transitionSchema), transitionSchema]))
     .optional()
     .describe(
       'The delayed transitions that will trigger after the specified delay'
     ),
   always: z
-    .array(transitionSchema)
+    .union([z.array(transitionSchema), transitionSchema])
     .optional()
     .describe(
       'The transitions that will immediately trigger if they are enabled; i.e. their guard evaluates to true'
@@ -109,6 +78,44 @@ export const stateSchema = z.object({
       .optional()
       .describe('The child states');
   },
+});
+
+export const atomicState = baseState.extend({
+  type: z.literal('atomic').optional(),
+  entry: z.array(actionSchema).optional(),
+  exit: z.array(actionSchema).optional(),
+  on: z
+    .record(z.string(), z.union([z.array(transitionSchema), transitionSchema]))
+    .optional(),
+  after: z
+    .record(z.string(), z.union([z.array(transitionSchema), transitionSchema]))
+    .optional(),
+  always: z.union([z.array(transitionSchema), transitionSchema]).optional(),
+  invoke: z.array(invokeSchema).optional(),
+});
+
+export const compoundState = baseState.extend({
+  type: z.literal('compound').optional(),
+  entry: z.array(actionSchema).optional(),
+  exit: z.array(actionSchema).optional(),
+  initial: z.string(),
+});
+
+export const parallelState = baseState.extend({
+  type: z.literal('parallel').optional(),
+  entry: z.array(actionSchema).optional(),
+  exit: z.array(actionSchema).optional(),
+  states: z.record(z.string(), stateSchema).optional(),
+});
+
+export const historyState = baseState.extend({
+  type: z.literal('history').optional(),
+  history: z.enum(['shallow', 'deep']).optional(),
+  target: z.string().optional(),
+});
+
+export const finalState = baseState.extend({
+  type: z.literal('final').optional(),
 });
 
 export const machineSchema = stateSchema.extend({
